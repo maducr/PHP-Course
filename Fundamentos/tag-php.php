6041764d4cb07bba6ed4821878bd4c36840e6697
maducr <crmariaeduarda2003@gmail.com>
2023-12-26 17:54:50 -0400

    Class files

---
+++ Fundamentos/tag-php.php
@@ -0,0 +1,9 @@
+<h1>Hi!!</h1>
+
+<?php
+
+    echo "<h2>Hello world!!!</h2>";
+
+?>
+
+<p>Hi!!</p>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+// ARQUIVO PHP - Se um arquivo tiver apenas código PHP é preferível que a tag de fechamento seja omitida
+
+/* TAG ABREVIADA (<?= ?>) */
+
+?>
